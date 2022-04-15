5f06573ad7d158b5c078b3fb515197f10145988e
hasan-mohammad-bd <hasan.md.east@gmail.com>
2022-04-15 16:03:02 +0600

    created route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Breakfast from './components/BreakFast/Breakfast';
+import Dinner from './components/Dinner/Dinner';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
+import Login from './components/Login/Login';
+import Lunch from './components/Lunch/Lunch';
+import Order from './components/Order/Order';
+import PageNotFound from './components/PageNotFound/PageNotFound';
+import Products from './components/Products/Products';
+import Signup from './components/Signup/Signup';
 
 function App() {
   return (
-    <div className="App">
-
+    <div>
+      <Header></Header>
+        <Routes>
+          <Route path="/" element={<Home></Home>}></Route>
+          <Route path="/home" element={<Home></Home>}></Route>
+          <Route path="*" element={<PageNotFound></PageNotFound>}></Route>
+          <Route path="/order" element={<Order></Order>}></Route>
+          <Route path="/login" element={<Login></Login>}></Route>
+          <Route path="/signup" element={<Signup></Signup>}></Route>
+          <Route path="/products" element={<Products></Products>}>
+            <Route path="/products/breakfast" element={<Breakfast></Breakfast>}></Route>
+            <Route path="/products/lunch" element={<Lunch></Lunch>}></Route>
+            <Route path="/products/dinner" element={<Dinner></Dinner>}></Route>
+          </Route>
+        </Routes>
+        <Footer></Footer>
     </div>
   );
 }
